703e43568e86eac60c1285a5561597d10c13c541
splexmus <pleang.ch@gmail.com>
2026-08-27 13:24:27 +0700

    feat: add registration api

--- apps/api/src/routes.ts
+++ apps/api/src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { subjectRouter } from "./router/subject.router.js";
+import { authRouter } from "./router/auth.router.js";
 
 export const apiRoutes = Router();
 
 apiRoutes.use("/subjects", subjectRouter);
+apiRoutes.use("/auth", authRouter);
